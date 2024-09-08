49445c56c066cb0725fb175b68afb32f6f50a598
Ihsan101 <96698644+Ihsan101@users.noreply.github.com>
2024-09-09 01:53:43 +0530

    Add files via upload

---
+++ php/addstake.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$passworddb = ""; 
+$dbname = "ayushtest"; 
+
+
+$conn = new mysqli($servername, $username, $passworddb, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$stake_id = $_POST['stake_id'];
+
+$sql = "SELECT * FROM stakeholderdata WHERE BINARY stakeid = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $stakeid); 
+$stmt->execute();
+$result = $stmt->get_result();
+
+
+
